65edef142426159cb96e956985ddb34799df6b4b
Benjamin897-glitch <franckenbenjamin@gmail.com>
2024-11-26 11:30:04 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+let getal = 0
+let maximum = 0
+let minimum = 0
+input.onButtonPressed(Button.A, function () {
+    getal = 0
+    getal += 1
+    maximum = 9
+})
+input.onButtonPressed(Button.B, function () {
+    getal += 0 - 1
+    minimum = 0
 })
